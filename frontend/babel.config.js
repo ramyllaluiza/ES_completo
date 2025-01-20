a09c9b9338a03934be825069216892cd66dca42a
ramyllaluiza <ramyllaluiza@gmail.com>
2025-01-19 23:58:27 -0300

    corrigindo erros no front

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   presets: [
-    "@babel/preset-env",
-    "@babel/preset-react",
+    "react-app",
+    "@babel/preset-env"
   ],
-};
+  plugins: [
+    [
+      "@babel/plugin-transform-class-properties",
+      { "loose": true }
+    ],
+    [
+      "@babel/plugin-transform-private-methods",
+      { "loose": true }
+    ],
+    [
+      "@babel/plugin-transform-private-property-in-object",
+      { "loose": true }
+    ]
+  ]
+}
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,5 +1,5 @@
 import { TextEncoder, TextDecoder } from 'util';
-
+import '@testing-library/jest-dom';
 // Adiciona TextEncoder e TextDecoder como polifills globalmente
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
